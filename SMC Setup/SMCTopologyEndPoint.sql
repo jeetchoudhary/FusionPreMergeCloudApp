954ca3ce8b36a3f36e0d73ff891bc88ad012da35
lazyverve <jeet.choudhary7@gmail.com>
2017-04-29 20:22:54 +0530

    Update SMC Commands

--- SMC Setup/SMCTopologyEndPoint.sql
+++ SMC Setup/SMCTopologyEndPoint.sql
@@ -5,7 +5,7 @@ DECLARE
  BEGIN
  ASK_TOPOLOGY_MANAGER.DEPLOY_ENTERPRISE_APPLICATION (
  p_enterprise_app_short_name => 'ORA_FSCM_UIAPP'
- , p_deployed_host => 'slc10gby.us.oracle.com'
+ , p_deployed_host => 'indl76040.in.oracle.com'
  , p_deployed_port => '7011'
  , p_environment_short_name => NULL
  , x_return_status => l_return_status
@@ -20,7 +20,7 @@ DECLARE
  
  ASK_TOPOLOGY_MANAGER.DEPLOY_ENTERPRISE_APPLICATION (
  p_enterprise_app_short_name => 'ORA_FSCM_SERVICESAPP'
- , p_deployed_host => 'slc10gby.us.oracle.com'
+ , p_deployed_host => 'indl76040.in.oracle.com'
  , p_deployed_port => '7011'
  , p_environment_short_name => NULL
  , x_return_status => l_return_status
@@ -34,7 +34,7 @@ DECLARE
  END IF;
  ASK_TOPOLOGY_MANAGER.DEPLOY_ENTERPRISE_APPLICATION (
  p_enterprise_app_short_name => 'ORA_FSCM_ESSAPP'
- , p_deployed_host => 'slc10gby.us.oracle.com'
+ , p_deployed_host => 'indl76040.in.oracle.com'
  , p_deployed_port => '7011'
  , p_environment_short_name => NULL
  , x_return_status => l_return_status
@@ -51,7 +51,7 @@ DECLARE
  ASK_TOPOLOGY_MANAGER.populate_end_point (
    logical_module_short_name =>  'ORA_FSCM_SERVICE'
  , deployed_protocol =>  'http'
- , deployed_host =>  'slc10gby.us.oracle.com'
+ , deployed_host =>  'indl76040.in.oracle.com'
  , deployed_port =>  '7011'
  , deployed_context_root =>  null
  , deployed_module_name =>  null
@@ -68,7 +68,7 @@ DECLARE
  ASK_TOPOLOGY_MANAGER.populate_end_point (
    logical_module_short_name =>  'ORA_FSCM_UI'
  , deployed_protocol =>  'http'
- , deployed_host =>  'slc10gby.us.oracle.com'
+ , deployed_host =>  'indl76040.in.oracle.com'
  , deployed_port =>  '7011'
  , deployed_context_root =>  null
  , deployed_module_name =>  null
@@ -85,7 +85,7 @@ DECLARE
  ASK_TOPOLOGY_MANAGER.populate_end_point (
    logical_module_short_name =>  'ORA_FSCM_ANALYTICS'
  , deployed_protocol =>  'http'
- , deployed_host =>  'slc10gby.us.oracle.com'
+ , deployed_host =>  'indl76040.in.oracle.com'
  , deployed_port =>  '7011'
  , deployed_context_root =>  null
  , deployed_module_name =>  null
@@ -102,7 +102,7 @@ DECLARE
  ASK_TOPOLOGY_MANAGER.populate_end_point (
    logical_module_short_name =>  'ORA_FSCM_REST'
  , deployed_protocol =>  'http'
- , deployed_host =>  'slc10gby.us.oracle.com'
+ , deployed_host =>  'indl76040.in.oracle.com'
  , deployed_port =>  '7011'
  , deployed_context_root =>  null
  , deployed_module_name =>  null
@@ -119,7 +119,7 @@ DECLARE
  ASK_TOPOLOGY_MANAGER.populate_end_point (
    logical_module_short_name =>  'ORA_FSCM_SEARCH'
  , deployed_protocol =>  'http'
- , deployed_host =>  'slc10gby.us.oracle.com'
+ , deployed_host =>  'indl76040.in.oracle.com'
  , deployed_port =>  '7011'
  , deployed_context_root =>  null
  , deployed_module_name =>  null
@@ -133,10 +133,62 @@ DECLARE
  ELSE
    DBMS_OUTPUT.PUT_LINE('ERROR: ' || l_msg_data);
  END IF;
-
- 
- 
  END;
  /
  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ##jitu Endpoints
+ ASK_TOPOLOGY_MANAGER.DEPLOY_ENTERPRISE_APPLICATION (
+ p_enterprise_app_short_name => 'ORA_FA_SOAAPP'
+ , p_deployed_host => 'indl76040.in.oracle.com'
+ , p_deployed_port => '7011'
+ , p_environment_short_name => NULL
+ , x_return_status => l_return_status
+ , x_msg_count => l_msg_count
+ , x_msg_data => l_msg_data  );
+ 
+ IF (l_return_status = ASK_TOPOLOGY_MANAGER.G_RET_STS_SUCCESS) THEN
+ DBMS_OUTPUT.PUT_LINE('SUCCESS: ' || l_msg_data);
+ ELSE
+ DBMS_OUTPUT.PUT_LINE('ERROR: ' || l_msg_data); 
+ END IF;
+ 
+ ASK_TOPOLOGY_MANAGER.DEPLOY_ENTERPRISE_APPLICATION (
+ p_enterprise_app_short_name => 'ORA_FA_WSMAPP'
+ , p_deployed_host => 'indl76040.in.oracle.com'
+ , p_deployed_port => '7011'
+ , p_environment_short_name => NULL
+ , x_return_status => l_return_status
+ , x_msg_count => l_msg_count
+ , x_msg_data => l_msg_data  );
+ 
+ IF (l_return_status = ASK_TOPOLOGY_MANAGER.G_RET_STS_SUCCESS) THEN
+ DBMS_OUTPUT.PUT_LINE('SUCCESS: ' || l_msg_data);
+ ELSE
+ DBMS_OUTPUT.PUT_LINE('ERROR: ' || l_msg_data); 
+ END IF;
+ 
+ ASK_TOPOLOGY_MANAGER.populate_end_point (
+   logical_module_short_name =>  'ORA_FASOAINTEGRATION'
+ , deployed_protocol =>  'http'
+ , deployed_host =>  'indl76040.in.oracle.com'
+ , deployed_port =>  '7011'
+ , deployed_context_root =>  null
+ , deployed_module_name =>  null
+ , environment_short_name =>  null
+ , x_return_status =>  l_return_status
+ , x_msg_count =>  l_msg_count
+ , x_msg_data =>  l_msg_data
+ );
+ IF (l_return_status = ASK_TOPOLOGY_MANAGER.G_RET_STS_SUCCESS) THEN
+   DBMS_OUTPUT.PUT_LINE('SUCCESS: ' || l_msg_data);
+ ELSE
+   DBMS_OUTPUT.PUT_LINE('ERROR: ' || l_msg_data);
+ END IF;
  
